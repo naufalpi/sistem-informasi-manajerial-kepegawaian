847b407c947f6a73c123bc1a3f9c9e5173fa163e
Naufal Akbar Pinasthika <nauakbar76@gmail.com>
2023-02-09 22:48:45 +0700

    Adding View Login & Regis

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -22,6 +22,11 @@
             <a class="nav-link {{ ($active === "categories") ? 'active' : '' }}" href="/categories">Categories</a>
           </li>
         </ul>
+        <ul class="navbar-nav ms-auto">
+          <div class="nav-item">
+            <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+          </div>
+        </ul>
       </div>
     </div>
 </nav>
